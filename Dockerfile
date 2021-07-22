3350619240066f62811b8986f353e03fb9105161
sam <sam.leider@hotmail.com>
2021-07-22 19:25:32 -0400

    added mongo to compose file

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,7 @@ COPY package.json .
 ARG NODE_ENV
 # if we are in development mode run npm install
 # if we are in production mode run npm install --only=production
+
 RUN if [ "$NODE_ENV" = "development" ]; \
     then npm install; \
     else npm install --only=production; \
--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { MONGO_IP, MONGO_PASSWORD, MONGO_USER, MONGO_PORT } = require('./config/config');
 
 const app = express();
 
+const MONGO_URL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+
+const connectWithRetry = (retryCount = 0) => {
+  mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  }).catch((err) => {
+    console.log('Error connecting to MongoDB', err);
+    if (retryCount < 5) {
+      console.log('Retrying connection in 5 seconds...');
+      setTimeout(() => {
+        retryCount += 1;
+        connectWithRetry(retryCount);
+      }, 5000);
+    } else {
+      console.log('Failed to connect to MongoDB');
+    }
+  });
+};
+
+connectWithRetry()
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
